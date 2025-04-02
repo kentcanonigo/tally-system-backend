7910a44460a375eda8ecf9d3102cf34452df11c3
laurenceibarra <laurencemibarra@gmail.com>
2025-04-02 11:43:30 +0800

    created the app and modules

--- backend/settings.py
+++ backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # API framework
+    'backend.api',  # API app
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import test_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test_api),
+    path('api/', include('backend.api.urls')), 
 ]
