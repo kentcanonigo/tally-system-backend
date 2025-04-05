7955277faba522c3772d6cf3775a7513a682bbf5
laurenceibarra <laurencemibarra@gmail.com>
2025-04-05 14:40:06 +0800

    Ready for backend logic

---
+++ backend/api/views/customers_views.py
@@ -0,0 +1,6 @@
+from backend.api.models.customers import Customers
+from backend.api.serializers.customers_serializer import CustomerSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
