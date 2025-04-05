fce8fa9de7f8610cf0d760bc913043e5a11daad0
laurenceibarra <laurencemibarra@gmail.com>
2025-04-05 15:16:19 +0800

    Ready for backend logic

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path("customers/", include("backend.api.urls.customers_urls")),
-    path("plants/", include("backend.api.urls.plants_urls")),
-    path("tally-sessions/", include("backend.api.urls.tally_sessions_urls")),
-    path("allocations/", include("backend.api.urls.allocation_details_urls")),
-    path("weight-classifications/", include("backend.api.urls.weight_classifications_urls")),
-]
