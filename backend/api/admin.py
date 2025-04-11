ce758810fa0f427650f9e655400f87de395a5a1b
laurenceibarra <laurencemibarra@gmail.com>
2025-04-11 13:53:55 +0800

    All models have their own CRUD and are also instantiated in the database

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from backend.api.models.customers import Customer
+from backend.api.models.plants import Plant
+from backend.api.models.weight_classifications import WeightClassification
+from backend.api.models.tally_sessions import TallySession
+from backend.api.models.allocation_details import AllocationDetails
 
 # Register your models here.
 admin.site.register([
     Customer,
+    Plant,
+    WeightClassification,
+    TallySession,
+    AllocationDetails,
 ])
--- backend/api/views/weight_classifications_views.py
+++ backend/api/views/weight_classifications_views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from backend.api.models.weight_classifications import WeightClassification
 from backend.api.serializers.weight_classifications_serializer import WeightClassificationSerializer
 
+
 class WeightClassificationViewSet(viewsets.ModelViewSet):
     queryset = WeightClassification.objects.all()
     serializer_class = WeightClassificationSerializer
