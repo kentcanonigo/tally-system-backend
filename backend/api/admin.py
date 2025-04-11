0d24664b0dad35a4a8b1d041c51eeb5beb161b3f
laurenceibarra <laurencemibarra@gmail.com>
2025-04-11 13:03:10 +0800

    Customer CRUD functionality

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from backend.api.models.customers import Customer
 
 # Register your models here.
+admin.site.register([
+    Customer,
+])
---
+++ backend/api/models/plants.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Plant(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
