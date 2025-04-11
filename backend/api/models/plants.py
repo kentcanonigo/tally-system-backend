7ad59732ce9fd6b4917a1ff6af6ece3589d9ea61
laurenceibarra <laurencemibarra@gmail.com>
2025-04-11 10:32:01 +0800

    for initial testing

--- backend/api/models/plants.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Plant(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField()
-
-    def __str__(self):
-        return self.name
